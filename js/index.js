87a543482bad47b9eb20a63a14e72a6822175109
HaCuong04 <tranhacuong090904@gmail.com>
2024-03-24 16:58:50 +0200

    fix bug 4

--- js/index.js
+++ js/index.js
@@ -1,83 +1,105 @@
-const BACKEND_ROOT_URL = 'https://todo-client-chud.onrender.com'
-import { Todos } from "./class/Todos.js" 
 
-const todos = new Todos(BACKEND_ROOT_URL)
+const BACKEND_ROOT_URL = 'https://todo-backend-k25z.onrender.com';
 
-const list = document.querySelector('ul')
-const input = document.querySelector('input')
+import { Todos } from './class/Todos.js';
 
-input.disabled = true
+const todos = new Todos(BACKEND_ROOT_URL);
+
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+input.disabled = true;
+
 
 const renderTask = (task) => {
-    const li = document.createElement('li')
-    li.setAttribute('class','list-group-item')
-    li.setAttribute('data-key',task.getId().toString())
-    // li.innerHTML = task.getText()
-    renderSpan(li,task.getText())
-    renderLink(li,task.getId())
-    list.append(li)
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item');
+    li.setAttribute('data-key', task.getId().toString());
+    // li.innerHTML = task.getText();
+    renderSpan(li, task.getText());
+    renderLink(li, task.getId());
+    list.append(li);
 }
 
-const renderSpan = (li,text) => {
-    const  span = li.appendChild(document.createElement('span'))
-    span.innerHTML = text
+const renderSpan = (li, text) => {
+    const span = li.appendChild(document.createElement('span'));
+    span.innerHTML = text;
 }
 
-const renderLink = (li,id) => {
-    const a = li.appendChild(document.createElement('a'))
-    a.innerHTML = '<i class="bi bi-trash"></i>'
-    a.setAttribute('style','float: right')
-    a.addEventListener('click',(event) => {
+const renderLink = (li, id) => {
+    const a = li.appendChild(document.createElement('a'));
+    a.innerHTML = '<i class="bi bi-trash"></i>';
+    a.setAttribute('style', 'float:right; cursor:pointer;');
+    a.addEventListener('click', (e) => {
         todos.removeTask(id).then((removed_id) => {
-            const li_to_remove = document.querySelector(`[data-key='${removed_id}']`)
-            if (li_to_remove) {
-                list.removeChild(li_to_remove)
+            const li_to_remove = document.querySelector(`[data-key="${removed_id}"]`);
+            if(li_to_remove) {
+                list.removeChild(li_to_remove);
             }
-        }).catch((error)=>{
-            alert(error)
+        }).catch((error) => {
+            console.error('Error:', error);
+            alert('Error removing task' + error);
         })
-    })
+    });
 }
 
 const getTasks = () => {
     todos.getTasks().then((tasks) => {
         tasks.forEach(task => {
-            renderTask(task)
-        })
-        input.disabled = false
+            renderTask(task);
+            
+        }
+        )
+        input.disabled = false;
     }).catch((error) => {
-        alert(error)
+        // console.error('Error:', error);
+        alert('Error fetching tasks' + error);
     })
+        // input.disabled = false;
 }
 
 const saveTask = async (task) => {
     try {
-        const json = JSON.stringify({description: task})
-        const response = await fetch(BACKEND_ROOT_URL+ '/new', {
+        const json = JSON.stringify({description: task});
+        const response = await fetch(BACKEND_ROOT_URL+ "/new", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: json
-        })
-        return response.json()
+        });
+        return response.json();
     } catch (error) {
-        alert('Error saving task#' + error.message)
+        console.error('Error:#', error);
+        alert('Error saving task#' + error.message);
     }
 }
 
-input.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter') {
-        event.preventDefault()
-        const task = input.value.trim()
-        if (task !=='') { 
-            todos.addTask(task).then((task)=> {
-                renderTask(task)
-                input.value = ''
-                input.focus()
+getTasks();
+
+button.addEventListener('click', () => {
+    const task = input.value.trim();
+    if (task !== '') {
+        todos.addTask(task).then((task) => {
+            renderTask(task);
+            input.value = '';
+            input.focus();
+        })
+    }
+}
+);
+
+input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        e.preventDefault();
+        const task = input.value.trim();
+        if (task !== '') {
+            todos.addTask(task).then((task) => {
+                renderTask(task);
+                input.value = '';
+                input.focus();
             })
         } 
     }
-})
-
-getTasks()
+});
